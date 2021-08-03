9f8d61b4bb23e5a2e53156f1ead553548ffb88d3
João PV Correia <correia.jpv@gmail.com>
2021-08-03 11:05:39 +0100

    dependencies and clean up

--- run_windows.bat
+++ run_windows.bat
@@ -4,13 +4,7 @@ Powershell.exe -executionpolicy bypass -File ./deps/windows/windows.ps1
 if errorlevel 1 pause & exit 
 
 if "%1"=="" (
-  echo running without menu
   pipenv run python ./src/init.py
-)
-
-if "%1"=="menu" (
-  echo running with menu
-  pipenv run python ./src/menu.py
 )
 
 echo exiting...
